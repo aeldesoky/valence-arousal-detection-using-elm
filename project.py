3363d2e1e419e8b53147875ef8ca127f3c6b9c14
Amir Eldesoky <aeldesok@unb.ca>
2018-11-02 14:56:24 -0300

    adding the Pipfile

--- project.py
+++ project.py
@@ -2,16 +2,17 @@ import pickle
 import os
 import numpy as np
 import matplotlib.pyplot as plt
+
 TIME = 60
-#data_file  = open(os.path.expanduser('s01.dat'), 'rb')
-#s01 = pickle.load(data_file, encoding='latin1')
-#pickle.dump(s01, open('test.txt', 'wb'))
+
+# data_file  = open(os.path.expanduser('~/Desktop/DEAP/data_preprocessed_python/s01.dat'), 'rb')
+# s01 = pickle.load(data_file, encoding='latin1')
+# pickle.dump(s01, open('test.txt', 'wb'))
 
 # write to file
 f = open('test.txt', 'rb')
 dic = pickle.load(f)
 f.close()
-
 
 y = dic['data']
 x = np.arange(0,TIME,TIME/len(y[1][1]))
@@ -28,8 +29,5 @@ plt.subplot(413)
 plt.plot(x,y[1][3])
 plt.subplot(414)
 plt.plot(x,y[1][4])
+plt.subplots_adjust(hspace=0.5)
 plt.show()
-
-
-#print((dic['labels']))
-#print((dic['data']))
