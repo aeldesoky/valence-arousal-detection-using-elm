9c52a2998f55486af13c1a5aba9d534546f7cf12
Amir Eldesoky <aeldesok@unb.ca>
2018-11-04 23:43:33 -0400

    plotting all channels in one loop

--- exploring_data.py
+++ exploring_data.py
@@ -12,18 +12,10 @@ f.close()
 
 y = dic['data']
 x = np.arange(0,TIME,TIME/len(y[1][1]))
-print(np.shape(y))
-print(len(y[1][1]))
 
-plt.subplot(411)
-plt.plot(x,y[1][1])
-plt.title('First Video')
-plt.ylabel('EEG')
-plt.subplot(412)
-plt.plot(x,y[1][2])
-plt.subplot(413)
-plt.plot(x,y[1][3])
-plt.subplot(414)
-plt.plot(x,y[1][4])
-plt.subplots_adjust(hspace=0.5)
+for i in range(1,40):
+   plt.plot(x,y[1][i])
+   plt.title('First Video')
+   plt.ylabel('EEG')
+
 plt.show()
