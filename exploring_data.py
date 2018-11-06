2f317365a4c54f0bdba36e7cd27b3508aff76b38
Amir Eldesoky <aeldesok@unb.ca>
2018-11-06 18:06:07 -0400

    changing the labels to classes

--- exploring_data.py
+++ exploring_data.py
@@ -4,9 +4,11 @@ import numpy as np
 import matplotlib.pyplot as plt
 import tensorflow as tf
 from tqdm import tqdm
+from loading_data import change_label_values_to_calss
 
 CHANNELS_NUM = 40
 VIDEOS_NUM   = 40
+CHANNEL_DATA_POINTS = 8064
 VIDEO_LENGTH_SECONDS = 60
 
 for file in sorted(os.listdir('data')):
@@ -16,21 +18,22 @@ for file in sorted(os.listdir('data')):
    data_dic = pickle.load(data_file)
    data_file.close()
 
-   all_channels_data = data_dic['data']
-   all_labels        = data_dic['labels']
-   valence_labels    = all_labels[:, 0].reshape(VIDEOS_NUM, 1)
-   arousal_labels    = all_labels[:, 1].reshape(VIDEOS_NUM, 1)
-   dominance_labels  = all_labels[:, 2].reshape(VIDEOS_NUM, 1)
-   liking_labels     = all_labels[:, 3].reshape(VIDEOS_NUM, 1)
+   all_channels_data  = data_dic['data']
+   all_labels_values  = data_dic['labels']
+   all_labels_classes = change_label_values_to_calss(all_labels_values)
+   valence_labels     = all_labels_values[:, 0].reshape(VIDEOS_NUM, 1)
+   arousal_labels     = all_labels_values[:, 1].reshape(VIDEOS_NUM, 1)
+   dominance_labels   = all_labels_values[:, 2].reshape(VIDEOS_NUM, 1)
+   liking_labels      = all_labels_values[:, 3].reshape(VIDEOS_NUM, 1)
 
-   time = np.arange(0,VIDEO_LENGTH_SECONDS, VIDEO_LENGTH_SECONDS/len(all_channels_data[1][1]))
+   time = np.arange(0, VIDEO_LENGTH_SECONDS, VIDEO_LENGTH_SECONDS/len(all_channels_data[1][1]))
 
-   for video_num in range(1,VIDEOS_NUM):
-      for channel_num in range(1,CHANNELS_NUM):
-         channel_data = all_channels_data[video_num][channel_num]
-         plt.plot(time,all_channels_data[video_num][channel_num])
+   for video_num in range(0, VIDEOS_NUM):
+      for channel_num in range(0, CHANNELS_NUM):
+         channel_data = all_channels_data[video_num][channel_num].reshape(CHANNEL_DATA_POINTS, 1)
+         plt.plot(time, channel_data)
       
-      plt.title('Video number ' + str(video_num) + ' (' + str(all_labels[video_num]) + ')')
-      plt.ylabel('EEG')
+      plt.title('Video number ' + str(video_num))
+      plt.ylabel('EEG Amplitude')
       plt.xlabel('Time in Seconds')   
       plt.show()
--- loading_data.py
+++ loading_data.py
@@ -4,18 +4,39 @@ import numpy as np
 import matplotlib.pyplot as plt
 from tqdm import tqdm
 
-directory_string = '~/Desktop/DEAP/data_preprocessed_python'
-directory = os.path.expanduser(directory_string)
-os.makedirs('data', exist_ok=True)
 
-print("Importing data...")
-for file in tqdm(sorted(os.listdir(directory))):
-   filename = os.fsdecode(file)
-   data_file_path = os.path.join(directory, filename)
-   if filename.endswith(".dat"):
-      data_file = open(data_file_path, 'rb')
-      pickle_file = pickle.load(data_file, encoding='latin1')
-      text_file = open(os.path.join('data/', os.path.splitext(filename)[0]) + ".txt", 'wb')
-      pickle.dump(pickle_file, text_file)
-      data_file.close()
-      text_file.close()
+def main():
+   directory_string = '~/Desktop/DEAP/data_preprocessed_python'
+   directory = os.path.expanduser(directory_string)
+   os.makedirs('data', exist_ok=True)
+
+   print("Importing data...")
+   for file in tqdm(sorted(os.listdir(directory))):
+      filename = os.fsdecode(file)
+      data_file_path = os.path.join(directory, filename)
+      if filename.endswith(".dat"):
+         data_file = open(data_file_path, 'rb')
+         pickle_file = pickle.load(data_file, encoding='latin1')
+         text_file = open(os.path.join('data/', os.path.splitext(filename)[0]) + ".txt", 'wb')
+         pickle.dump(pickle_file, text_file)
+         data_file.close()
+         text_file.close()
+
+
+def change_label_values_to_calss (all_labels):
+   temp_labels = np.empty((40,4), dtype=object)
+
+   for i in range(0, len(all_labels)):
+      for j in range(0, np.size(all_labels, 1)):
+         if(all_labels[i][j] <= 5):
+            temp_labels[i][j] = 'L'
+         else:
+            temp_labels[i][j] = 'H '
+   
+   emotions_label = np.array([['V', 'A', 'D', 'L']] * 40)
+
+   return temp_labels + emotions_label
+
+
+if __name__ == "__main__":
+   main()
