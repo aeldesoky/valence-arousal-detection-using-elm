d33f9afcf67e024b149b095bd614fc23732d216c
Amir Eldesoky <aeldesok@unb.ca>
2018-11-06 20:13:32 -0400

    fft

--- exploring_data.py
+++ exploring_data.py
@@ -3,7 +3,7 @@ import os
 import numpy as np
 import matplotlib.pyplot as plt
 import tensorflow as tf
-from tqdm import tqdm
+from scipy.fftpack import fft, fftfreq
 from loading_data import change_label_values_to_calss
 
 CHANNELS_NUM = 40
@@ -26,14 +26,19 @@ for file in sorted(os.listdir('data')):
    dominance_labels   = all_labels_values[:, 2].reshape(VIDEOS_NUM, 1)
    liking_labels      = all_labels_values[:, 3].reshape(VIDEOS_NUM, 1)
 
-   time = np.arange(0, VIDEO_LENGTH_SECONDS, VIDEO_LENGTH_SECONDS/len(all_channels_data[1][1]))
+   time = np.arange(0, VIDEO_LENGTH_SECONDS, VIDEO_LENGTH_SECONDS/CHANNEL_DATA_POINTS)
+   frequency = np.linspace(0, 128, CHANNEL_DATA_POINTS)
+
+   print(np.shape(frequency))
 
    for video_num in range(0, VIDEOS_NUM):
       for channel_num in range(0, CHANNELS_NUM):
          channel_data = all_channels_data[video_num][channel_num].reshape(CHANNEL_DATA_POINTS, 1)
-         plt.plot(time, channel_data)
+         channel_data_fft = fft(channel_data)
+         plt.plot(frequency, abs(channel_data_fft))
+         plt.plot()
       
-      plt.title('Video number ' + str(video_num))
-      plt.ylabel('EEG Amplitude')
-      plt.xlabel('Time in Seconds')   
-      plt.show()
+         plt.title('Video number ' + str(video_num))
+         plt.ylabel('EEG Amplitude')
+         plt.xlabel('Time in Seconds')   
+         plt.show()
